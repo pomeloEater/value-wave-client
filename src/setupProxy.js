fd77d3dad3617476aaca20751ed424821b0730e4
jinhui.dev <jinhui.dev@gmail.com>
2022-05-18 10:09:14 +0900

    modify proxy 설정 주석처리 react+nginx로 분리할 경우 활성화할 것

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const options = {
-  target: 'http://localhost:8000',
-  pathFilter: ['/api/**', '/hello'],
-  changeOrigin: true,
-};
-module.exports = function (app) {
-  app.use(createProxyMiddleware(options));
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// const options = {
+//   target: 'http://localhost:8000',
+//   pathFilter: ['/api/**', '/hello'],
+//   changeOrigin: true,
+// };
+// module.exports = function (app) {
+//   app.use(createProxyMiddleware(options));
+// };
