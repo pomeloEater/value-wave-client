c61ac42faa71ec1f41a564d2e605ef0674fb9ffd
jinhui.dev <jinhui.dev@gmail.com>
2022-06-02 18:29:06 +0900

    Feat: 부동산종합정보 모달창 기본기능 완료

--- src/components/modal/BasicModal.js
+++ src/components/modal/BasicModal.js
@@ -6,46 +6,67 @@ import {
   HiOutlineChevronDown,
   HiOutlineChevronUp,
 } from 'react-icons/hi';
+import Draggable from 'react-draggable';
 
 const ModalContainer = styled.div`
   position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
   z-index: 1000;
+  pointer-events: none;
 `;
 
 // const ModalDimLayer = styled.div``;
 
 const ModalWrapper = styled.div`
-  background-color: var(--color-gray-100);
   display: flex;
   flex-direction: column;
-  min-width: 30rem;
-  min-height: 30rem;
+  position: absolute;
+  min-width: 300px;
+  width: 600px;
+  height: auto;
+  max-width: 100%;
+  max-height: inherit;
+  right: calc(50% - 300px);
+  top: 0;
+  background-color: var(--color-gray-100);
+  border: 1px solid var(--color-gray-300);
+  box-shadow: var(--shadow-md);
+  pointer-events: auto;
 `;
 
 const ModalHeader = styled.div`
   display: flex;
-  background-color: green;
+  padding: 0.2rem 0.5rem;
+  background-color: var(--color-gray-100);
+  border-bottom: 1px solid var(--color-gray-300);
 `;
 
-const ModalTitleWrapper = styled.h3`
+const ModalTitleWrapper = styled.h4`
   flex-grow: 1;
+  margin-left: 0.5rem;
+  cursor: move;
 `;
 
-const ModalButtonWrapper = styled.div``;
+const ModalButtonWrapper = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  & svg {
+    width: 20px;
+    height: 30px;
+  }
+`;
 
 const ModalContent = styled.div`
-  padding: 1rem;
-  background-color: yellow;
+  padding: 0.5rem;
+  background-color: var(--color-white);
+  /* overflow-y: auto; */
 `;
 
 const BasicModal = props => {
-  const { title, children, closeModal } = props;
+  const { width, height, title, children, closeModal } = props;
   const ref = useRef(null);
   const [displayed, setDisplayed] = useState(false);
   const [folded, setFolded] = useState(false);
@@ -63,26 +84,28 @@ const BasicModal = props => {
   if (ref.current && displayed) {
     return createPortal(
       <ModalContainer>
-        <ModalWrapper>
-          <ModalHeader>
-            <ModalTitleWrapper>{title}</ModalTitleWrapper>
-            <ModalButtonWrapper>
-              {folded ? (
-                <HiOutlineChevronDown onCLick={toggleModal} />
-              ) : (
-                <HiOutlineChevronUp onCLick={toggleModal} />
-              )}
-              <HiOutlineX onClick={closeModal} />
-            </ModalButtonWrapper>
-          </ModalHeader>
-          {folded ? (
-            <></>
-          ) : (
-            <>
-              <ModalContent>{children}</ModalContent>
-            </>
-          )}
-        </ModalWrapper>
+        <Draggable>
+          <ModalWrapper style={{ width: { width }, height: { height } }}>
+            <ModalHeader>
+              <ModalTitleWrapper>{title}</ModalTitleWrapper>
+              <ModalButtonWrapper>
+                {folded ? (
+                  <HiOutlineChevronDown onClick={toggleModal} />
+                ) : (
+                  <HiOutlineChevronUp onClick={toggleModal} />
+                )}
+                <HiOutlineX onClick={closeModal} />
+              </ModalButtonWrapper>
+            </ModalHeader>
+            {folded ? (
+              <></>
+            ) : (
+              <>
+                <ModalContent>{children}</ModalContent>
+              </>
+            )}
+          </ModalWrapper>
+        </Draggable>
       </ModalContainer>,
       ref.current
     );
